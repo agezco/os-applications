c6f6ee33ae6b93a2e812d61ea8b6a03efa8cb79e
Steffen Hönig <s.hoenig@openthinclient.com>
2016-03-07 14:15:07 +0000

    [tcos-libs] new Class: Cmd()

--- tcos-libs/tcos-libs-2.1/package-rootfs/usr/lib/pymodules/python2.7/pytcos/tcos.py
+++ tcos-libs/tcos-libs-2.1/package-rootfs/usr/lib/pymodules/python2.7/pytcos/tcos.py
@@ -23,6 +23,7 @@
 
 import base64
 import commands
+import copy
 import ldap
 import ldap.filter
 import ldapurl
@@ -187,7 +188,7 @@ class Util(Logger):
         x = os.getenv('firstscreenWidth')
         y = os.getenv('firstscreenHeigth')
         if x == "" or y == "":
-            return FLASE
+            return False
         if withouttaskbar == True:
             if commands.getoutput("pidof mate-panel"):
                 panel_auto_hide = commands.getoutput("dconf read /org/mate/panel/toplevels/top/auto_hide")
@@ -211,7 +212,7 @@ class Util(Logger):
         x = os.getenv('secondscreenWidth')
         y = os.getenv('secondscreenHeigth')
         if x == "" or y == "":
-            return FLASE
+            return False
         #
         # CAVEAT: How should we know the y-offset?
         #
@@ -1467,3 +1468,69 @@ class Launcher(Logger):
         else:
             # do something about the missing stuff
             return {}
+
+    def mergeWithLdap(self, target, source=None):
+        '''
+        merges two ENTRY dicts where source values win over the one's from target
+        
+        options = dict()
+        # Key -> Config name, list: 0 -> default value additional, 1-> non-ldap data
+        options = {
+            'Config.Option1': ['bla', 'real_bla'],
+            'Config.Option3': ['wurst']
+            }
+        l = tcos.Launcher()
+        l.mergeWithLdap(OPTIONS)  
+        '''
+        if not source:
+            source = self.ENTRY
+
+        for k in source.keys():
+            if k in target:
+                target[k][0] = source[k]
+            else:
+                target[k] = [source[k]]
+
+class Cmd(Logger):
+    def __init__(self, exe=None):
+        # self.LOG is filled and needed by Logger.log()
+        self.LOG = []
+        self._path = []
+        if exe:
+            self._path = Cmd._to_list(self._path, exe)
+        pass
+
+    def __call__(self, args=''):
+        return self.run(args)
+
+    def add(self, new):
+        '''adds arguments to the self._path'''
+        self._path = Cmd._to_list(self._path, new)
+
+    @staticmethod
+    def _to_list(_list, new):
+        if type(new) == list:
+            _list.extend(new)
+        elif type(new) == str:
+            _list.extend(new.split())
+        else:
+            _list.append(str(new))
+        return _list
+
+    @property
+    def path(self):
+        return self._path
+
+    def run(self, args=''):
+        '''args will not be stored permanently'''
+        # throw away _path after run
+        _path = copy.deepcopy(self._path)
+        _path = Cmd._to_list(_path, args)
+        logging = Logger()
+        logging.log(1, str(_path))
+        try:
+            p = subprocess.Popen(_path, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        except TypeError:
+            raise
+        else:
+            return p
